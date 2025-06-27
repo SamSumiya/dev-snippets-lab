e460b7177e3f3ac8037d04a22370d8a4c25d02b1
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-06-27 15:36:19 -0700

    modified polling function to make it testable

--- snippets/polling.ts
+++ snippets/polling.ts
@@ -1,12 +1,20 @@
 // Basic Pollling with loop 
 
-const delay = (ms: number): Promise<void> => new Promise<void>(resolve => setTimeout(resolve, ms))
+type FetchFn = () => Promise<{status: number, json: () => Promise<{ready: boolean}>}>
+type DelayFn = (ms: number) => Promise<void>  
 
-async function polling(maxAttempts: number = 5) {
+const delayFn = (ms: number): Promise<void> => new Promise<void>(resolve => setTimeout(resolve, ms))
+
+async function polling(
+    delayFn: DelayFn, 
+    fetchFn: FetchFn, 
+    maxAttempts: number = 5,
+    delayMS: 2000
+) {
     let count: number = 0 
 
     while ( count < maxAttempts ) {
-        const response = await fetch('./data')
+        const response = await fetchFn()
 
         if (response.status === 200) {
             const data = await response.json()
@@ -18,9 +26,44 @@ async function polling(maxAttempts: number = 5) {
 
         count++ 
         if (count < maxAttempts) {
-            await delay(2000)
+            await delayFn(delayMS)
         }
     }
 
     throw new Error('Polling timed out after maximum attempts')
+}
+
+let attempt = 0 
+
+async function fetchFn() {
+    attempt++
+    console.log(`Fetch attempt ${attempt}`)
+    return {
+        status: 200,
+        json: async () => ({
+            ready: attempt >= 3, 
+            message: `This is attempt ${attempt}`,
+            timestamp: Date.now()
+        })
+    }
+}
+
+// Using new Promise 
+// Promise.resolve(...) also works 
+function fetchFN() {
+    attempt++
+    return new Promise((resolve) => {
+        resolve({
+            status: 200, 
+            json: () => {
+                return new Promise((resolve) => {
+                    resolve({
+                        ready: attempt >= 3, 
+                        message: `This is attempt ${attempt}`,
+                        timestamp: Date.now()
+                    })
+                })
+            }
+        })
+    })
 }
